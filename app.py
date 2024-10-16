ffa88b939ba7cf36c0a45310c5dba34ef879a41b
ChethanNCI <x23297395@student.ncirl.ie>
2024-10-16 10:54:15 +0000

    Styling Done

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+import json
 app = Flask(__name__)
 @app.route("/")
 def hello_world():
